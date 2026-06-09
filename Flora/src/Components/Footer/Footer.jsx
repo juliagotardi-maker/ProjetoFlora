dc92a387517372b421d8d063f23cd8c9c8f9e48f
Julia Gotardi <julia.gotardi@aluno.senai.br>
2026-06-09 08:56:24 -0300

    lk

--- Flora/src/Components/Footer/Footer.jsx
+++ Flora/src/Components/Footer/Footer.jsx
@@ -4,24 +4,15 @@ export default function Footer() {
   return (
     <footer className="footer">
 
-      <div className="footer-info">
-        <h3>Flora 🌷</h3>
-        <p>Flores, plantas e presentes para momentos especiais.</p>
-      </div>
+      <h2>Flora</h2>
 
-      <div className="footer-links">
-        <h4>Categorias</h4>
-        <p>Buquês</p>
-        <p>Plantas</p>
-        <p>Vasos</p>
-        <p>Presentes</p>
-      </div>
+      <p>
+        Flores frescas para tornar cada momento especial.
+      </p>
 
-      <div className="footer-contato">
-        <h4>Contato</h4>
-        <p>(14) 99999-9999</p>
-        <p>contato@flora.com</p>
-      </div>
+      <span>
+        © 2026 Flora - Todos os direitos reservados.
+      </span>
 
     </footer>
   );
